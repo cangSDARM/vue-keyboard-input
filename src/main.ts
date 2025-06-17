792f4f24971cc487b8e71aaeecc9e584603c6e22
cangSDARM <648384410li@gmail.com>
2025-06-17 11:40:04 +0800

    fix: ts type

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
+// @ts-ignore
 import vuetify from './vuetify';
+// @ts-ignore
 import App from './App.vue'
 
 createApp(App).use(vuetify).mount('#app')
